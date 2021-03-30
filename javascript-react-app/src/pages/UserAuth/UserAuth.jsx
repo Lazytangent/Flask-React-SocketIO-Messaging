439cddf9196321d131c927cf53726c9411d5243b
Peter Mai <peter.tn.mai@gmail.com>
2021-03-30 09:18:48 -0500

    Finish UserAuth forms

--- javascript-react-app/src/pages/UserAuth/UserAuth.jsx
+++ javascript-react-app/src/pages/UserAuth/UserAuth.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
+import { login, signUp } from '../../store/session';
 import FormHeader from '../../components/FormHeader';
 import FormErrors from '../../components/FormErrors';
 import FormField from '../../components/FormField';
@@ -41,21 +42,30 @@ const UserAuth = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    let user;
+    if (location.pathname === '/login') {
+      user = await dispatch(login(credential, password));
+    } else {
+      user = await dispatch(signUp(username, email, password, confirmPassword));
+    }
+    if (user.errors) {
+      setErrors(user.errors);
+    }
   };
 
   if (location.pathname === '/login') {
     return (
-      <>
+      <form onSubmit={submitHandler}>
         <FormHeader heading='Login' />
         <FormErrors errors={errors} />
         <FormField type='text' name='credential' placeholder='Username or Email' required={true} value={credential} onChange={updateCredential} />
         <FormField type='password' name='password' placeholder='Password' required={true} value={password} onChange={updatePassword} />
         <Button label="Login" />
-      </>
+      </form>
     )
   } else if (location.pathname === '/signup') {
     return (
-      <>
+      <form onSubmit={submitHandler}>
         <FormHeader heading='Signup' />
         <FormErrors errors={errors} />
         <FormField type='text' name='username' placeholder='Username' required={true} value={username} onChange={updateUsername} />
@@ -63,7 +73,7 @@ const UserAuth = () => {
         <FormField type='password' name='password' placeholder='Password' required={true} value={password} onChange={updatePassword} />
         <FormField type='password' name='confirmPassword' placeholder='Confirm Password' required={true} value={confirmPassword} onChange={updateConfirmPassword} />
         <Button label="Signup" />
-      </>
+      </form>
     )
   }
 };
