21a6514814e6286172b8653a67f81f4cffd7cf5e
Lazytangent <pm123633@gmail.com>
2021-03-29 22:27:50 -0500

    Getting started with TS

---
+++ typescript-react-app/src/store/index.ts
@@ -0,0 +1,36 @@
+import {
+  applyMiddleware, compose, createStore, combineReducers,
+} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import sessionReducer from './session.ts';
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+interface HydratedState {
+  session: {
+    user: {
+      username: string;
+      email: string;
+      id: number;
+    },
+  },
+}
+
+const configureStore = (preloadedState: HydratedState) => {
+  createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
---
+++ typescript-react-app/src/store/session.ts
@@ -0,0 +1,53 @@
+const SET_SESSION: string = 'session/SET_SESSION';
+const REMOVE_SESSION: string = 'session/REMOVE_SESSION';
+
+interface User {
+  username: string;
+  email: string;
+  id: number;
+}
+
+interface SetUserAction {
+  type: string;
+  user: User;
+}
+
+interface RemoveAction {
+  type: string;
+}
+
+const setSession = (user: User): SetUserAction => ({
+  type: SET_SESSION,
+  user,
+});
+
+const removeSession = (): RemoveAction => ({
+  type: REMOVE_SESSION,
+});
+
+export const authenticate = () => async (dispatch) => {
+  const res: Response = await fetch('/api/auth');
+  const user: User = await res.json();
+  if (!user.errors) {
+    dispatch(setSession(user));
+  }
+  return user;
+};
+
+export const login = (credential: string, password: string) => async (dispatch) => {
+  const res: Response = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application-json',
+    },
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const user: User = await res.json();
+  if (!user.errors) {
+    dispatch(setSession(user));
+  }
+  return user;
+};
