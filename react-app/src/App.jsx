1ea842c90956ca0e06d85514d3bb2a45428f416d
Lazytangent <pm123633@gmail.com>
2021-03-29 21:39:42 -0500

    Create routing

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
+import UserAuth from './components/UserAuth';
+import Messenger from './components/Messenger';
 
 import { authenticate } from './store/session';
 
@@ -14,26 +17,17 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Messenger />
+      </Route>
+      <Route path="/login">
+        <UserAuth />
+      </Route>
+      <Route path="/signup">
+        <UserAuth />
+      </Route>
+    </Switch>
   );
 };
 
