b7bdb73fc411c74b8ddc7f4b65faf9c8eecbb6c2
Lazytangent <pm123633@gmail.com>
2021-03-29 20:48:57 -0500

    Update index.jsx

--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './store';
+
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
 
 const Root = () => (
-  <App />
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 ReactDOM.render(
