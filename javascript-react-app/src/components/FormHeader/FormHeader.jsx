4ebe218f88a57217aa4560c8571f7e7a90b22bf3
Lazytangent <pm123633@gmail.com>
2021-03-30 14:42:54 -0500

    ESLint FormHeader

--- javascript-react-app/src/components/FormHeader/FormHeader.jsx
+++ javascript-react-app/src/components/FormHeader/FormHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './FormHeader.module.css';
 
@@ -7,5 +8,15 @@ const FormHeader = ({ heading, className }) => (
     <h3>{heading}</h3>
   </div>
 );
+
+FormHeader.defaultProps = {
+  heading: 'Default Heading',
+  className: '',
+};
+
+FormHeader.propTypes = {
+  heading: PropTypes.string,
+  className: PropTypes.string,
+};
 
 export default FormHeader;
