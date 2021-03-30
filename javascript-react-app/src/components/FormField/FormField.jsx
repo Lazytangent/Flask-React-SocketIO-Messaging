b014edad24fcdbe5da9f78fc109dd8f5c112c2fe
Lazytangent <pm123633@gmail.com>
2021-03-30 13:17:43 -0500

    ESLint FormField

--- javascript-react-app/src/components/FormField/FormField.jsx
+++ javascript-react-app/src/components/FormField/FormField.jsx
@@ -1,20 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './FormField.module.css';
 
-const FormField = ({ className, type, name, placeholder, required, value, onChange }) => {
+const FormField = ({
+  className,
+  type,
+  name,
+  placeholder,
+  required,
+  value,
+  onChange,
+}) => {
   if (required) {
     return (
-    <>
-      <input className={`${className ? className : ''} ${styles.input}`}required type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
-    </>
-    );
-  } else {
-    return (
       <>
-        <input className={`${className ? className : ''} ${styles.input}`} type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
+        <input
+          className={`${className || ''} ${styles.input}`}
+          required
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
       </>
     );
   }
+  return (
+    <>
+      <input
+        className={`${className || ''} ${styles.input}`}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+FormField.defaultProps = {
+  className: '',
+  type: 'text',
+  name: 'default',
+  placeholder: 'Default Input Field',
+  required: false,
+  value: '',
+  onChange: () => null,
+};
+
+FormField.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  onChange: PropTypes.func,
 };
 
 export default FormField;
