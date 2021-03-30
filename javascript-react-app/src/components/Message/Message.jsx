5ee5dce697c7c80dec0ec06d16ccc99fa263d35a
Lazytangent <pm123633@gmail.com>
2021-03-30 14:52:15 -0500

    ESLint Message

--- javascript-react-app/src/components/Message/Message.jsx
+++ javascript-react-app/src/components/Message/Message.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Message.module.css';
 
@@ -10,5 +11,21 @@ const Message = ({ message }) => (
     <p>{message.created_at}</p>
   </div>
 );
+
+Message.defaultProps = {
+  message: {
+    body: '',
+    sender: '',
+    created_at: '',
+  },
+};
+
+Message.propTypes = {
+  message: PropTypes.shape({
+    body: PropTypes.string,
+    sender: PropTypes.string,
+    created_at: PropTypes.string,
+  }),
+};
 
 export default Message;
