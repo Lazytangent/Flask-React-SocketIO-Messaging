132d10abac8af89ced152b225801f2477ce6750f
Peter Mai <peter.tn.mai@gmail.com>
2021-03-30 10:15:02 -0500

    Update MessageContainer

--- javascript-react-app/src/components/MessageContainer/MessageContainer.jsx
+++ javascript-react-app/src/components/MessageContainer/MessageContainer.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
+import { getMessages } from '../../store/messages';
 import styles from './MessageContainer.module.css';
 import Message from '../Message';
 
 const MessageContainer = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
   const messages = useSelector((state) => state.messages.messages);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getMessages(user.id));
+    }
+  }, [user, dispatch]);
 
   return (
     <div className={styles.div}>
