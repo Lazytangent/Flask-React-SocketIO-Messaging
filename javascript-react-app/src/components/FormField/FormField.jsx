1c9ace0aaa0cc9ba119dc118604c443fb2b14bf6
Peter Mai <peter.tn.mai@gmail.com>
2021-03-30 08:41:44 -0500

    FormField component

--- javascript-react-app/src/components/FormField/FormField.jsx
+++ javascript-react-app/src/components/FormField/FormField.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './FormField.module.css';
 
-const FormField = () => (
-  <>
-    <h3>Placeholder for FormField</h3>
-  </>
-);
+const FormField = ({ className, type, name, placeholder, required, value, onChange }) => {
+  if (required) {
+    return (
+    <>
+      <input className={`${className ? className : ''} ${styles.input}`}required type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
+    </>
+    );
+  } else {
+    return (
+      <>
+        <input className={`${className ? className : ''} ${styles.input}`} type={type} name={name} placeholder={placeholder} value={value} onChange={onChange} />
+      </>
+    );
+  }
+};
 
 export default FormField;
