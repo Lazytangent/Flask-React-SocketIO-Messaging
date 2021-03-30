ac25077f6f20f39fbc57c2be340d477a3c7e418d
Peter Mai <peter.tn.mai@gmail.com>
2021-03-30 09:06:42 -0500

    UserAuth page

--- javascript-react-app/src/pages/UserAuth/UserAuth.jsx
+++ javascript-react-app/src/pages/UserAuth/UserAuth.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-const UserAuth = () => (
-  <>
-    <h3>Placeholder for UserAuth</h3>
-  </>
-);
+import FormHeader from '../../components/FormHeader';
+import FormField from '../../components/FormField';
+import Button from '../../components/Button';
+
+const UserAuth = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const updateCredential = (e) => {
+    setCredential(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  if (location.pathname === '/login') {
+    return (
+      <>
+        <FormHeader heading='Login' />
+        <FormField type='text' name='credential' placeholder='Username or Email' required={true} value={credential} onChange={updateCredential} />
+        <FormField type='password' name='password' placeholder='Password' required={true} value={password} onChange={updatePassword} />
+        <Button label="Login" />
+      </>
+    )
+  }
+
+};
 
 export default UserAuth;
