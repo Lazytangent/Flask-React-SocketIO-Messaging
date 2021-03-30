484ebf817e5c1542d038dab5a3f9111b1c189e4f
Lazytangent <pm123633@gmail.com>
2021-03-30 12:42:53 -0500

    ESLint Button component

--- javascript-react-app/src/components/Button/Button.jsx
+++ javascript-react-app/src/components/Button/Button.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './Button.module.css';
 
 const Button = ({ className, label }) => (
-  <button className={`${className} ${styles.button}`}>
+  <button type="submit" className={`${className} ${styles.button}`}>
     {label}
   </button>
 );
+
+Button.defaultProps = {
+  className: '',
+  label: 'Button',
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+};
 
 export default Button;
