0a859621d7989dc825b4dcf440dbca0654509dc2
Lazytangent <pm123633@gmail.com>
2021-03-30 20:50:17 -0500

    Basic SocketIO config

---
+++ javascript-react-app/src/socketIO/index.js
@@ -0,0 +1,26 @@
+import io from 'socket.io-client';
+
+let socket;
+
+export const initiateSocket = () => {
+  socket = io('http://localhost:5000');
+  console.log('Connecting socket...');
+  console.dir(socket);
+};
+
+export const disconnectSocket = () => {
+  console.log('Disconnecting socket...');
+  if (socket) socket.disconnect();
+};
+
+export const subscribeToChat = (cb) => {
+  if (!socket) return true;
+  socket.on('message', msg => {
+    console.log('Websocket event received!');
+    return cb(null, msg);
+  });
+};
+
+export const sendMessage = (message) => {
+  if (socket) socket.emit('message', { message });
+};
