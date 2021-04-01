276f2bcbbd44578090abdf42dcecf4efabbf8dce
Lazytangent <pm123633@gmail.com>
2021-03-31 19:25:21 -0500

    Fix typo

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,7 +6,7 @@ from app.models import db, User
 def seed_users():
     new_users = []
     with open('./app/seeds/users.json') as f:
-        data = json.loads(f)
+        data = json.load(f)
         for user in data:
             new_user = User(**user)
             new_users.append(new_user)
