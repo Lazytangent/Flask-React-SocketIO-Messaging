438b5ebdaa88f886d14e98a4e79f9f0281ef5109
Lazytangent <pm123633@gmail.com>
2021-04-05 16:01:50 -0500

    Add UserSelector to messenger page

--- javascript-react-app/src/pages/Messenger/Messenger.jsx
+++ javascript-react-app/src/pages/Messenger/Messenger.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import MessageContainer from '../../components/MessageContainer';
 import MessageForm from '../../components/MessageForm';
+import UserSelector from '../../components/UserSelector';
 
 const Messenger = () => (
   <>
+    <UserSelector />
     <MessageContainer />
     <MessageForm />
   </>
