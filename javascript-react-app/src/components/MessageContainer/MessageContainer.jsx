ade06e9fd7fbce1c08f97cde51f836bb43b4d2cf
Lazytangent <pm123633@gmail.com>
2021-04-05 16:30:40 -0500

    Sorting logic

--- javascript-react-app/src/components/MessageContainer/MessageContainer.jsx
+++ javascript-react-app/src/components/MessageContainer/MessageContainer.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getMessages } from '../../store/messages';
-import {
-  useUserContext,
-} from '../../context/UserContext';
+import { useUserContext } from '../../context/UserContext';
 import styles from './MessageContainer.module.css';
 import Message from '../Message';
 
 const MessageContainer = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
-  const messages = useSelector((state) => state.messages.messages);
+  const messages = useSelector((state) => Object.values(state.messages.messages).sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at),
+  ));
 
   const { userState } = useUserContext();
 
