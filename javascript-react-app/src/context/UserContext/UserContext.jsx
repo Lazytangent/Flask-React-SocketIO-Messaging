a74bd5bddd0a5dd6e16e29dfbf2f2c70515553ee
Lazytangent <pm123633@gmail.com>
2021-04-05 14:47:14 -0500

    Fix typos

--- javascript-react-app/src/context/UserContext/UserContext.jsx
+++ javascript-react-app/src/context/UserContext/UserContext.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { SET_CONVERSATION_USER, REMOVE_CONVERSATION_USER } from './constants';
 
-const UsersReducer = (state, action) => {
+const UserReducer = (state, action) => {
   switch (action.type) {
     case SET_CONVERSATION_USER:
       return { user: action.user };
@@ -18,28 +18,28 @@ const initialState = {
   currentUser: null,
 };
 
-const UsersContext = createContext();
+const UserContext = createContext();
 
-const useUsersContext = () => useContext(UsersContext);
+const useUserContext = () => useContext(UserContext);
 
-const UsersProvider = ({ children }) => {
-  const [usersState, dispatch] = useReducer(UsersReducer, initialState);
+const UserProvider = ({ children }) => {
+  const [usersState, dispatch] = useReducer(UserReducer, initialState);
 
   return (
-    <UsersContext.Provider value={{ usersState, dispatch }}>
+    <UserContext.Provider value={{ usersState, dispatch }}>
       {children}
-    </UsersContext.Provider>
+    </UserContext.Provider>
   );
 };
 
-UsersProvider.defaultProps = {
+UserProvider.defaultProps = {
   children: null,
 };
 
-UsersProvider.propTypes = {
+UserProvider.propTypes = {
   children: PropTypes.node,
 };
 
-export { useUsersContext };
+export { useUserContext };
 
-export default UsersProvider;
+export default UserProvider;
