f76c7cf4df881562f213e35482e3bbf1586773d1
Lazytangent <pm123633@gmail.com>
2021-04-05 14:20:09 -0500

    Write basic component

--- javascript-react-app/src/components/UserSelector/UserSelector.jsx
+++ javascript-react-app/src/components/UserSelector/UserSelector.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const UserSelector = () => (
-  <>
-    <h3>Placeholder for UserSelector</h3>
-  </>
-);
+import { getUsers } from '../../store/users';
+
+const UserSelector = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => Object.values(state.users));
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h3>Placeholder for UserSelector</h3>
+      {users.map((user) => (
+        <h3>{user.username}</h3>
+      ))}
+    </>
+  );
+};
 
 export default UserSelector;
