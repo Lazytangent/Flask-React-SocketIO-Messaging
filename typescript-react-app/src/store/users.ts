a6a5082d6b9a78e0faa3df08f31776f4e80a4a77
Lazytangent <pm123633@gmail.com>
2021-04-12 10:51:36 -0500

    Users slice of state

--- typescript-react-app/src/store/users.ts
+++ typescript-react-app/src/store/users.ts
@@ -1,5 +1,24 @@
+import { AppDispatch, User } from '../types';
+
 const SET_USERS: string = 'users/SET_USERS';
 const SET_USER: string = 'users/SET_USER';
 const REMOVE_USER: string = 'users/REMOVE_USER';
 
-const setUsers = (users)
+interface UserObject {
+  [key: number]: User;
+}
+
+const setUsers = (users: UserObject) => ({
+  type: SET_USERS,
+  users,
+});
+
+const setUser = (user: User) => ({
+  type: SET_USER,
+  user,
+});
+
+const removeUser = (userId: number) => ({
+  type: REMOVE_USER,
+  userId,
+});
